8c60ef04b20a1f0b2729ca683542f0684f023df2
Vasily Zorin <kak.serpom.po.yaitsam@gmail.com>
2019-01-13 17:47:40 +0700

    Proxy

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # socket.io-proxy
+
+node socketio-proxy.js \
+    --url='wss://url/to/websocket/' \
+    --listen=3000 \
+    --verbose
+    
--- socketio-proxy.js
+++ socketio-proxy.js
@@ -18,15 +18,15 @@ var connections = [];
 router.use(function (socket, args, next) {
     socket.ws = connections[socket.id] || null;
     var event = args[0];
-    console.log('C2S', args);
+    argv.verbose && console.log('C2S', args);
     if (socket.ws == null || !socket.ws.connected) {
         if (buffers[socket.id] == null) {
             buffers[socket.id] = [];
         }
-        console.log('C2S: Buffering ' + event);
+        argv.verbose && console.log('C2S: Buffering ' + event);
         buffers[socket.id].push(args);
     } else {
-        console.log('C2S: Sending ' + event);
+        argv.verbose && console.log('C2S: Sending ' + event);
         socket.ws.send(JSON.stringify(args));
     }
     next();
@@ -43,18 +43,18 @@ io.on('connection', function (socket) {
     var ws = new WebSocketClient();
 
     ws.on('connectFailed', function (error) {
-        socket.log('Connect Error: ' + error.toString());
+        argv.verbose && socket.log('Connect Error: ' + error.toString());
         socket.disconnect();
     });
 
     ws.on('connect', function (connection) {
-        socket.log('connected to ' + argv['url']);
+        argv.verbose && socket.log('connected to ' + argv['url']);
         connections[socket.id] = connection;
         if (buffers[socket.id] != null) {
             buffers[socket.id].forEach(function(item) {
                 let event = item[0];
                 let data = item[1];
-                socket.log('C2S: Sending ' + event + ' from buffer');
+                argv.verbose && socket.log('C2S: Sending ' + event + ' from buffer');
                 connection.send(JSON.stringify([event, data]));
             });
             delete buffers[socket.id];
@@ -62,23 +62,23 @@ io.on('connection', function (socket) {
 
         connection.on('error', function (error) {
             socket.disconnect();
-            socket.log("Connection Error: " + error.toString());
+            argv.verbose && socket.log("Connection Error: " + error.toString());
         });
         connection.on('close', function () {
             socket.disconnect();
-            socket.log('echo-protocol Connection Closed');
+            argv.verbose && socket.log('echo-protocol Connection Closed');
         });
         connection.on('message', function (message) {
             if (message.type === 'utf8') {
                 var event = JSON.parse(message.utf8Data);
-                socket.log('received', event);
+                argv.verbose && socket.log('received', event);
                 socket.emit.apply(socket, JSON.parse(message.utf8Data));
             }
         });
     });
 
-    socket.log('connecting to ' + argv['url']);
-    ws.connect(argv['url'], 'echo-protocol');
+    argv.verbose && socket.log('connecting to ' + argv.url);
+    ws.connect(argv.url, 'echo-protocol');
 });
 
-proxy.listen(argv['listen']);
+proxy.listen(argv.listen);
